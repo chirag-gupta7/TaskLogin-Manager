63574f09a7602aeb385fbc26ad671eedfb545038
chirag-gupta7 <chirag15470956@gmail.com>
2026-07-07 13:28:18 +0530

    added features to get all and single tasks

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import engine, Base, get_db
 import models
@@ -12,6 +12,18 @@ app = FastAPI()
 @app.get('/health')
 def health():
     return{"status":"OK"}
+
+@app.get("/api/v1/tasks", response_model=list[schema.TaskResponce])
+def list_All_Tasks(db: Session = Depends(get_db)):
+    tasks = db.query(models.Task).all()
+    return tasks
+
+@app.get("/api/v1/tasks/{id}", response_model= schema.TaskResponce)
+def get_Task(id: int, db: Session = Depends(get_db)):
+    task = db.query(models.Task).filter(models.Task.id == id).first()
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return task
 
 @app.post("/api/v1/tasks", response_model=schema.TaskResponce)
 def create_task(task: schema.TaskCreate, db: Session = Depends(get_db)):
